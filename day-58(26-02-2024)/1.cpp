c1dc3de15c4486c14eda1af86470fa2a0b8b2a59
Krishna Pandey <krish36pandey@gmail.com>
2024-02-26 22:35:39 +0530

    day-58

---
+++ day-58(26-02-2024)/1.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL)
+            return true;
+        if((p==NULL && q!=NULL) || (p!=NULL && q== NULL))
+            return false;
+        if(p->val != q->val)
+           return false;
+        bool left = isSameTree(p->left,q->left);
+        bool right = isSameTree(p->right,q->right);
+        return left&&right;
+        
+    }
+};
