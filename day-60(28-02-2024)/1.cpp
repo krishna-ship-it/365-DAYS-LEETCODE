3fce2990d1e0980c8be61e103b92ad6356c02e8e
Krishna Pandey <krish36pandey@gmail.com>
2024-02-28 23:10:01 +0530

    day-60

---
+++ day-60(28-02-2024)/1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        int ans =  -1;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                auto f = q.front();
+                q.pop();
+                if(i==0)
+                    ans = f->val;
+                if(f->left)
+                    q.push(f->left);
+                if(f->right)
+                    q.push(f->right);    
+            }
+        }
+        return ans;
+    }
+};
